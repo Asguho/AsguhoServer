3cfc03abaf5640bbeb0d16a4490919790e3a621a
Asguho <78410939+Asguho@users.noreply.github.com>
2023-06-13 21:56:30 +0200

    update

--- modinstaller.pl
+++ modinstaller.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/perl
 use strict;
-my @modrinthMods = ('Lithium','LazyDFU','ferrite-core','starlight','Krypton','memoryleakfix', 'smoothboot-fabric', 'debugify','c2me-fabric','no-chat-reports','fastload','vmp-fabric','servercore', 'carpet','quickcarpet', 'syncmatica', 'inventory-sorting','fabric-api' );
-my @curseforgeMods = ('Servux' );
+my @modrinthMods = ('Lithium','LazyDFU','ferrite-core','starlight','Krypton','memoryleakfix', 'smoothboot-fabric', 'debugify','c2me-fabric','no-chat-reports','fastload','vmp-fabric','servercore', 'carpet','quickcarpet', 'syncmatica', 'inventory-sorting','fabric-api', 'Servux');
+#my @curseforgeMods = ('' );
 
 
 for(@modrinthMods){
 	system("packwiz mr install $_ -y");
 }
 
-for(@curseforgeMods){
-	system("packwiz cf install $_ -y");
-}
+#for(@curseforgeMods){
+#	system("packwiz cf install $_ -y");
+#}
 
 
 
